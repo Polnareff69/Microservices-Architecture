43f6b18c61217bda05b97e72b9ed0d580172309e
Polnareff <munozgomezjuanjose69@gmail.com>
2025-04-27 19:30:26 -0500

    Inicio del proyecto

---
+++ CreacionBaseDeDatosExchangeSystem.sql
@@ -0,0 +1,17 @@
+CREATE TABLE accounts (
+    IdCuenta UUID PRIMARY KEY,
+    idUsuario UUID NOT NULL,
+    credits INTEGER,
+    creationDate TIMESTAMP WITHOUT time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE transactions (
+    idtransaction UUID PRIMARY KEY,
+    from_acc UUID NOT NULL,
+    to_acc UUID NOT NULL,
+    amount INTEGER,
+    currency INTEGER,
+	creationDate TIMESTAMP WITHOUT time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
