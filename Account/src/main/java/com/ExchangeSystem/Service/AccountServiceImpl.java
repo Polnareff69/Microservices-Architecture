ecff191fd24c37515c4b01fbd37a7fc551a714d7
Polnareff <munozgomezjuanjose69@gmail.com>
2025-04-27 21:11:52 -0500

    Se terminan de implementar las clases del microservicio

--- Account/src/main/java/com/ExchangeSystem/Service/AccountServiceImpl.java
+++ Account/src/main/java/com/ExchangeSystem/Service/AccountServiceImpl.java
@@ -3,12 +3,12 @@ package com.ExchangeSystem.Service;
 
 import com.ExchangeSystem.Entity.Account;
 import com.ExchangeSystem.Repository.AccountRepository;
-import com.account.grpc.AccountGrpc;
-import com.account.grpc.AccountRequest;
-import com.account.grpc.AccountResponse;
+import com.account.grpc.*;
 import io.grpc.stub.StreamObserver;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.lang.ref.ReferenceQueue;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @GrpcService
@@ -31,5 +31,92 @@ public class AccountServiceImpl extends AccountGrpc.AccountImplBase{
                 .build();
         responseObserver.onNext(accResponse);
         responseObserver.onCompleted();
+        System.out.println("Bien ahi");
+        }
+
+    @Override
+    public void updateAccountBalance(UpdateAccountRequest request, StreamObserver<UpdateAccountResponse> responseObserver) {
+        UUID accountId = UUID.fromString(request.getIdAccount());
+        long newBalance = request.getCredits();
+        Account acc = accountRepository.findByIdAccount(accountId);
+
+        // Verifica si existe
+        if (acc != null) {
+            acc.setCredits((int) newBalance);  // setter
+            accountRepository.save(acc); // actualizar
+            UpdateAccountResponse response = UpdateAccountResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("Precio actualizado correctamente")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
+            UpdateAccountResponse response = UpdateAccountResponse.newBuilder()
+                    .setSuccess(false)
+                    .setMessage("Símbolo no encontrado")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
     }
-}
+
+    @Override
+    public void createAccount(CreateAccountRequest request, StreamObserver<CreateAccountResponse> responseObserver) {
+        try {
+            // Verificar si la cuenta ya existe
+            Account existing = accountRepository.findByIdAccount(UUID.fromString(request.getIdAccount()));
+            if (existing != null) {
+                CreateAccountResponse response = CreateAccountResponse.newBuilder()
+                        .setSuccess(false)
+                        .setMessage("Ya existe una cuenta con este Id")
+                        .build();
+                responseObserver.onNext(response);
+                responseObserver.onCompleted();
+                return;
+            }
+
+            // Crear nueva cuenta
+            Account acc = new Account();
+            acc.setIdAccount(UUID.fromString(request.getIdAccount()));
+            //Temporalmente el mismo Id
+            acc.setIdUser(UUID.fromString(request.getIdAccount()));
+            acc.setCredits((int) request.getCredits());
+            acc.setCreationDate(LocalDateTime.parse(LocalDateTime.now().toString()));
+            accountRepository.save(acc);
+            CreateAccountResponse response = CreateAccountResponse.newBuilder()
+                    .setSuccess(true)
+                    .setMessage("Cuenta creada exitosamente.")
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            e.printStackTrace();
+            CreateAccountResponse response = CreateAccountResponse.newBuilder()
+                    .setSuccess(false)
+                    .setMessage("Error al crear la cuenta: " + e.getMessage())
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
